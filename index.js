2594f3f5878c0eb413d741dc62a1de93b0baaab1
youqiang <714508719@qq.com>
2022-04-17 20:17:02 +0800

    格式化文件

--- index.js
+++ index.js
@@ -1,60 +1,75 @@
-const path = require('path');
-const process =require('child_process');
-const fs= require('fs');
+const path = require("path");
+const process = require("child_process");
+const fs = require("fs");
 
 // git项目路径
-const project_path = 'D:\\Desktop\\gitFiles\\shop.zhengan-food.com\\app';
-const current_path = __dirname;
-const changed_desc_name = 'changed_file.txt';
-// 临时文件路径
-const target_file = path.join(current_path,changed_desc_name);
+const PROJECT_PATH = "D:\\Desktop\\gitFiles\\shop.zhengan-food.com";
+// diff文件路径
+const CURRENT_PATH = __dirname;
+const CHANGED_DESC_NAME = "changed_file.txt";
+const DIFF = path.join(CURRENT_PATH, CHANGED_DESC_NAME);
 // 复制后的文件路径
-const tmp_path = 'tmp_files';
+const TEMP_PATH = "tmp_files";
 const is_log = true;
 
-function copy(src,dst){
-    fs.copyFileSync(src,dst)
+function copy(src, dst) {
+    let dirname = path.dirname(dst);
+    // 创建对应的文件夹
+    if (!fs.existsSync(dirname)) fs.mkdirSync(dirname, { recursive: true });
+    // 不处理文件夹
+    if (dst.endsWith("/")) return;
+    if (dst.endsWith("\\")) return;
+    fs.copyFileSync(src, dst);
 }
 
-function log(){
-    if(is_log) console.log(...arguments)
+function log() {
+    if (is_log) console.log(...arguments);
 }
 
-function main(){
-    // 创建临时的文件
-    process.exec(`cd ${project_path} && git status > "${target_file}"`)
+function main() {
+    new Promise((resolve, reject) => {
+        // 创建临时的文件
+        process.exec(
+            `cd ${PROJECT_PATH} && git status > "${DIFF}"`,
+            (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            }
+        );
+    })
+    .then(() => {
+        let diff = fs.readFileSync(DIFF, "utf8").split("\n");
+        let res = [];
+        diff.forEach((element) => {
+            if (element.startsWith("\t")) res.push(element);
+        });
 
-    let file = fs.readFileSync(target_file, 'utf8').split('\n')
-    let res = [];
-    file.forEach(element => {
-        // if(element.startWith('\t'))
-        if(element.startsWith('\t'))
-            res.push(element)
-    });
-
-    for(let i in res)
-    {
-        // 去掉\t
-        res[i] = res[i].replace(/\t/g,'')
-        // 去掉 modified   
-        res[i] = res[i].replace(/modified:   /g,'')
-        // 获取真实路径
-        res[i] = 
-        res[i] = {
-            path : path.resolve(project_path,res[i]),
-            _path : res[i]
+        for (let i in res) {
+            // 去掉\t
+            res[i] = res[i].replace(/\t/g, "");
+            // 去掉 modified
+            res[i] = res[i].replace(/modified:   /g, "");
+            // 去除 ../
+            res[i] = res[i].replace(/\.\.\//g, "");
+            // 获取真实路径
+            res[i] = {
+                path: path.resolve(PROJECT_PATH, res[i]),
+                _path: res[i],
+            };
         }
-    }
-    // log(res);
-    log('total files:',res.length);
-    let target_path = '';
-    for(let i in res){
-        target_path = path.join(current_path,tmp_path,res[i]._path);
-        target_dirname = path.dirname(target_path);
-        // 创建文件路径
-        if(!fs.existsSync(target_dirname)) fs.mkdirSync(target_dirname,{recursive:true})
-        log('copy: ',res[i].path,' to ',target_path);
-        copy(res[i].path, target_path);
-    }
+        log("total files:", res.length);
+        let target_path = "";
+        for (let i in res) {
+            target_path = path.join(CURRENT_PATH, TEMP_PATH, res[i]._path);
+            log(`copy [${Number(i)+1}]: ${res[i].path} to ${target_path}`);
+            copy(res[i].path, target_path);
+        }
+    })
+    .catch((e) => {
+        log(e);
+    });
 }
-main(); 
+main();
