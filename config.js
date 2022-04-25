1819c12ebe6fd42b2ecf64091eadf4ff22c0602e
jazzg62 <714508719@qq.com>
2022-04-25 08:25:53 +0800

    支持配置文件

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = [
+    // {
+    //     "name": "config",  // 项目名称
+    //     "project_path":"D:\\work\\git\\github\\vue-cli3-template\\",  // 项目路径
+    //     "target_path": "D:\\work\\git\\github\\vue-cli3-template\\config.js",  // 复制后的文件路径，可选项，默认为 当前路径\name
+    // }
+]
--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const path = require("path");
 const process = require("child_process");
 const fs = require("fs");
 
-// git项目路径
-const PROJECT_PATH = "D:\\Desktop\\test";
-// diff文件路径
-const CURRENT_PATH = __dirname;
-const CHANGED_DESC_NAME = "changed_file.txt";
-const DIFF = path.join(CURRENT_PATH, CHANGED_DESC_NAME);
-// 复制后的文件路径
-const TEMP_PATH = "tmp_files";
 const is_log = true;
 
+/**
+ * 复制文件
+ * @param {String} src 源文件路径
+ * @param {String} dst 复制后的文件路径
+ * @returns 
+ */
 function copy(src, dst) {
     let dirname = path.dirname(dst);
     // 创建对应的文件夹
@@ -22,54 +20,89 @@ function copy(src, dst) {
     fs.copyFileSync(src, dst);
 }
 
+/**
+ * 记录日志
+ */
 function log() {
     if (is_log) console.log(...arguments);
 }
 
-function main() {
-    new Promise((resolve, reject) => {
-        // 创建临时的文件
-        process.exec(
-            `cd \\D ${PROJECT_PATH} && git status > "${DIFF}"`,
-            (error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            }
-        );
-    })
-    .then(() => {
-        let diff = fs.readFileSync(DIFF, "utf8").split("\n");
-        let res = [];
-        diff.forEach((element) => {
-            if (element.startsWith("\t")) res.push(element);
-        });
-
-        for (let i in res) {
-            // 去掉\t
-            res[i] = res[i].replace(/\t/g, "");
-            // 去掉 modified
-            res[i] = res[i].replace(/modified:   /g, "");
-            // 去除 ../
-            res[i] = res[i].replace(/\.\.\//g, "");
-            // 获取真实路径
-            res[i] = {
-                path: path.resolve(PROJECT_PATH, res[i]),
-                _path: res[i],
-            };
-        }
-        log("total files:", res.length);
-        let target_path = "";
-        for (let i in res) {
-            target_path = path.join(CURRENT_PATH, TEMP_PATH, res[i]._path);
-            log(`copy [${Number(i)+1}]: ${res[i].path} to ${target_path}`);
-            copy(res[i].path, target_path);
+/**
+ * 解析diff文本
+ * @param {*} statusText 
+ * @returns {Array}
+ */
+function parse(statusText){
+    let arr = statusText.split("\n");
+    let res = [];
+    arr.forEach((el)=>{
+        if(el.startsWith("\t")){
+            res.push(el);
         }
     })
-    .catch((e) => {
-        log(e);
-    });
+    for (let i in res) {
+        // 去掉\t
+        res[i] = res[i].replace(/\t/g, "");
+        // 去掉 modified
+        res[i] = res[i].replace(/modified:   /g, "");
+        // 去除 ../
+        res[i] = res[i].replace(/\.\.\//g, "");
+    }
+    return res;
 }
+
+/**
+ * 清空路径下的文件
+ * @param {String} path 
+ */
+function clear(path){
+    if(!fs.existsSync(path))
+        return ;
+    log('clear path:', path);
+    return ;
+    try{
+        fs.rmdirSync(path);
+        fs.mkdirSync(path);
+    }catch(e){
+        log(e);
+    }
+}
+
+function main(){
+    const config = require('./config')
+    log(`total projects: ${config.length}`);
+    for(let item of config){
+        let project_path = item['project_path'];
+        let project_name = item['name'];
+        let target_path = item['target_path'];
+        new Promise((resolve, reject) => {
+            if(!fs.existsSync(project_path))
+                reject("project path is not exists");
+            let ps = process.exec(
+                `cd /D ${project_path} && git status `,
+                (error) => {
+                    if (error) reject(error);
+                }
+            );
+            ps.stdout.on('data',function(data){
+                resolve(data);
+            })
+        })
+        .then((data) => {
+            let res = parse(data);
+            log("total files:", res.length);
+            let source_file = "", target_file = "";
+            for (let i in res) {
+                source_file = path.resolve(project_path, res[i]);
+                target_file= path.join(target_path, res[i]);
+                log(`[${project_name}] copy [${Number(i)+1}]: ${source_file} to ${target_file}`);
+                copy(source_file, target_file);
+            }
+        })
+        .catch((e) => {
+            log(e);
+        });
+    }
+}
+
 main();
