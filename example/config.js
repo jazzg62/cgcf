345a06b1b97e036ceba8118f0298c6d6a9c3ab3f
jazzg62 <714508719@qq.com>
2022-05-09 13:51:53 +0800

    1

--- example/config.js
+++ example/config.js
@@ -1,7 +1,7 @@
 module.exports = [
-    {
-        "name": "config",  // 项目名称
-        "project_path":"F:\\Desktop\\cgcf-test",  // 项目路径
-        "target_path": "F:\\Desktop\\cgcf\\tmp",  // 复制后的文件路径，可选项，默认为 当前路径\name
-    }
+    // {
+    //     "name": "config",  // 项目名称
+    //     "project_path":"F:\\Desktop\\cgcf-test",  // 项目路径
+    //     "target_path": "F:\\Desktop\\cgcf\\tmp",  // 复制后的文件路径，可选项，默认为 当前路径\name
+    // }
 ]
